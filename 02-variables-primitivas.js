2e96a6b1b4689d6a0466c9a6d9303f76e126eea4
Ferponce1996 <fernando.ponce@epn.edu.com>
2019-10-23 15:19:59 -0500

    cambio

---
+++ 02-variables-primitivas.js
@@ -0,0 +1,7 @@
+const numero = 10
+const decimal = 10.12
+const upsups  = typeof numero // para saber que tipo de variable es 
+
+console.log(upsups)  // para imprimir 
+//gestor de paquetes
+// de node js
---
+++ 02-variables-primitivas.ts
@@ -0,0 +1,6 @@
+conts letra = "a"
+const palabra = "palabra"
+const hijos = null
+console.log(typepf hijos);
+const casado = false ;
+console.log(typeof casado);
