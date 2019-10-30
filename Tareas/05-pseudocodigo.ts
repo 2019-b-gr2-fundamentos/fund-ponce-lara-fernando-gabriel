7e6fe965bb01bf6e8b8aa8c6546bf5f510f1e1ea
Ferponce1996 <fernando.ponce@epn.edu.com>
2019-10-30 16:08:19 -0500

    ofi

--- Tareas/05-pseudocodigo.ts
+++ Tareas/05-pseudocodigo.ts
@@ -1,12 +1,12 @@
 /*
  Crear un programa donde al insertar 2 numeros por teclado a y b .
- Muestre las opciones + , - , * , / , promedio
+ Muestre las opciones + , - , * , / , promedio : al insertar la letra inicial de dicha operacion.
  y que la operacion se escoja por teclado y el resultado se imprima por pantalla
 
 
 1.Insertar A
 2.Insertar B
-3.Escoger la operacion
+3.Escoger la operacion con la letra s ,r , m , d ,p 
 4.Realizar la operacion y guardar el resultado
 5.imprimir resultado por pantalla
 
