3f5d4356762deed8eb0b6c79ddae3a4b0c105ad8
Ferponce1996 <fernando.ponce@epn.edu.com>
2019-10-30 16:05:23 -0500

    actualizado

---
+++ Tareas/05-pseudocodigo.ts
@@ -0,0 +1,21 @@
+/*
+ Crear un programa donde al insertar 2 numeros por teclado a y b .
+ Muestre las opciones + , - , * , / , promedio
+ y que la operacion se escoja por teclado y el resultado se imprima por pantalla
+
+
+1.Insertar A
+2.Insertar B
+3.Escoger la operacion
+4.Realizar la operacion y guardar el resultado
+5.imprimir resultado por pantalla
+
+*/
+
+
+
+
+
+
+
+*/
