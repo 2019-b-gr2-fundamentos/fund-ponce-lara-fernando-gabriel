c4bb251e555c8366615a7d264a66534394468224
Fernandito <55715279+Ferponce1996@users.noreply.github.com>
2019-12-26 16:59:34 -0500

    kjkj

---
+++ Curso C Edx/1.1.2_saltos de linea.c
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+
+int main(void){
+	
+	
+	printf("hola mundffo");
+	printf("como estas?");
+	
+	return 0;
+	
+}
---
+++ Curso C Edx/main.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+
+int main(void){
+	
+	
+	printf("hola muikikndo");
+	printf("como estas?");
+	
+	return 0;
+	
+}
+
